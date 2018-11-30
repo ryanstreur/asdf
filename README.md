cbca30180a833ebfd65975bad73b679582f6c7b5
Ryan Streur <ryan.streur@bluevolt.com>
2018-11-29 17:27:24 -0800

    add a command, a jshintrc, and a README

---
+++ README.md
@@ -0,0 +1,8 @@
+# asdf
+
+*an ergonomic cli*
+
+## Next steps
+
+- Add config file support to let user decide where to put their log messages
+- Add config file support to let user decide what format their log messages should be in
---
+++ index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const fs = require('fs');
+const moment = require('moment');
+const request = require('request');
+const htmlMetadata = require('html-metadata');
+
+program
+  .version('0.0.0')
+  .command('log <string>')
+  .alias('l')
+  .action(function (string) {
+    fs.appendFile('C:/Users/ryan.streur/Dropbox/notes/log.org', getLogString(string), function (err, res) {
+      if (err && err.code === 'ENOENT') {
+        fs.writeFile('~/Dropbox/notes/log.org', string, function (err) {
+          console.log(err);
+        });
+        console.log('file created: ~/Dropbox/notes/log.org');
+      } else if (err) {
+        console.log(err);
+      } else {
+        console.log('logged msg: ', string);
+      }
+    });
+  });
+
+const urlDestMarkdown = 'C:/Users/ryan.streur/Dropbox/bloggo/content/post/2018/articles-week-of-11-26.md';
+program.command('url <url>')
+  .alias('u')
+  .action(url => {
+    htmlMetadata(url).then(metadata => {
+      const title = metadata.general.title;
+      const lineToAppend = `* [${title}](${url})\n`;
+      fs.appendFile(urlDestMarkdown, lineToAppend, e => console.log(e));
+      console.log(`Appended ${lineToAppend} to file ${urlDestMarkdown}`);
+    });
+  });
+
+program.parse(process.argv);
+
+function getLogString(str) {
+  const timestamp = moment().format('YYYY-MM-DD ddd kk:mm');
+  return `* ${str}\n  <${timestamp}>\n`;
+}
