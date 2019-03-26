4d0153b28678f8411c8daf5b3613bb7b6d45d556
Ryan Streur <rjstreur@gmail.com>
2019-03-26 08:38:42 -0700

    enable url command to accept arrays of arguments rather than just single urls

--- cmds/url.command.js
+++ cmds/url.command.js
@@ -11,12 +11,15 @@ module.exports = {
 const urlDestMarkdown = 'C:/Users/ryan.streur/Dropbox/bloggo/content/post/2018/articles-week-of-11-26.md';
 function url(url) {
   config.getSetting('url', config => {
-    htmlMetadata(url).then(metadata => {
-      const title = metadata.general.title;
-      const lineToAppend = `* [${title}](${url})\n`;
-      const filePath = config.filePath[0] === '~' ? getUserHome() + config.filePath.slice(1) : config.filePath;
-      fs.appendFile(filePath, lineToAppend, e => console.log(e));
-      console.log(`Appended ${lineToAppend} to file ${urlDestMarkdown}`);
-    });
+    const urlArray = url.split(' ');
+      urlArray.forEach(url => {
+          htmlMetadata(url).then(metadata => {
+              const title = metadata.general.title;
+              const lineToAppend = `* [${title}](${url})\n`;
+              const filePath = config.filePath[0] === '~' ? getUserHome() + config.filePath.slice(1) : config.filePath;
+              fs.appendFile(filePath, lineToAppend, e => console.log(e));
+              console.log(`Appended ${lineToAppend} to file ${urlDestMarkdown}`);
+          });
+      });
   });
 }
